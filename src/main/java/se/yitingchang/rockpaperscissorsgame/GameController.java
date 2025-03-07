bdb64573d8c00966d45152e291e43bc4a266bf9a
Yiting Chang <yiting.chang@iths.se>
2025-03-07 09:39:59 +0100

    Update GameController.java

--- src/main/java/se/yitingchang/rockpaperscissorsgame/GameController.java
+++ src/main/java/se/yitingchang/rockpaperscissorsgame/GameController.java
@@ -1,4 +1,31 @@
 package se.yitingchang.rockpaperscissorsgame;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping("/")
+        public String play(Model model) {
+        return "play";
+    }
+
+    @PostMapping("/play")
+    public String playGame(@RequestParam String choice, Model model) {
+        gameService.play(choice);
+        model.addAttribute("playerChoice", gameService.getPlayerChoice());
+        model.addAttribute("computerChoice", gameService.getComputerChoice());
+        model.addAttribute("result", gameService.getResult());
+        return "play";
+    }
+
 }
